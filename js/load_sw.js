f9bed05cbda03b5ef5bccf069c1e43e8e6bbd1fd
john <john.mouawad.aziz@gmail.com>
2018-03-25 15:13:43 +0200

    edit the load_sw.js

--- js/load_sw.js
+++ js/load_sw.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', { scope: '/' })
   .then(function(registration) {
-    console.log('Service Worker Registered');
+    console.log('Service Worker Registered succissfully');
   });
   navigator.serviceWorker.ready.then(function(registration) {
-   console.log('Service Worker Ready');
+   console.log('Service Worker Ready now');
  });
 }
